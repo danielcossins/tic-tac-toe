a41fca3254d6b444831fbd1c135980074b7f8367
danielcossins11 <daniel.cossins@yahoo.com>
2015-07-23 15:02:48 -0500

    top row checks for win

--- main.js
+++ main.js
@@ -6,13 +6,16 @@ $('td').click(function(){
 });
 
 function checkForWin () {
-
-
-  if ($("#4").hasClass("blue") === $("#5").hasClass("blue") && $("#4").hasClass("blue") === $("#3").hasClass("blue")) {
-  	alert("hey!!!!!")
-  } 
-
-
-
-
+  if ($("#1").hasClass("red") === $("#2").hasClass("red") && 
+  	$("#1").hasClass("red") === $("#3").hasClass("red")) {
+  	alert("hey!!!!!");
+  }
+  if ($("#4").hasClass("red") === $("#5").hasClass("red") && 
+  	$("#4").hasClass("red") === $("#6").hasClass("red")) {
+  	alert("hey!!!!!");
+  }
+  if ($("#7").hasClass("red") === $("#8").hasClass("red") && 
+  	$("#7").hasClass("red") === $("#9").hasClass("red")) {
+  	alert("hey!!!!!");
+  }
 }
