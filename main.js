18f339cc6380cb3941f1aad2dc307088fc399814
Caitlin Stein <caitlin.teter@gmail.com>
2015-07-23 14:41:09 -0500

    added id's to td's

--- main.js
+++ main.js
@@ -2,6 +2,17 @@
 $('td').click(function(){
   $(this).addClass("blue");
   $(this).toggleClass("red");
-
-  
+  checkForWin();
 });
+
+function checkForWin () {
+
+
+  if ($("#4").hasClass("blue") === $("#5").hasClass("blue") && $("#4").hasClass("blue") === $("#3").hasClass("blue")) {
+  	alert("hey!!!!!")
+  } 
+
+
+
+
+}
